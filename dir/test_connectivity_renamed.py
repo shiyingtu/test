121223d1a78f0ebe30a4255d793b337556f813f5
britneytu <alynn0801@hotmail.com>
2023-04-25 17:20:40 -0700

    Update test_connectivity_renamed.py

--- dir/test_connectivity_renamed.py
+++ dir/test_connectivity_renamed.py
@@ -1,5 +1,5 @@
 """
-This playbook runs the 'test connectivity' action on all possible assets currently configured.
+This playbook runs theerw 'test connectivity' action on all possible assets currently configured.
 """
 
 import phantom.rules as phantom
